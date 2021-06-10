49e80cb87319412c81079d831e7933278ddbb7be
Katarzyna Gawrys <katarzyna.gawrys@polsl.pl>
2021-06-10 09:51:36 +0200

    usuniecie jednego testu

--- src/test/java/pl/zajecia/dodawanie/DodawanieApplicationTest.java
+++ src/test/java/pl/zajecia/dodawanie/DodawanieApplicationTest.java
@@ -1,10 +1,12 @@
 package pl.zajecia.dodawanie;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DodawanieApplicationTest {
 
-    @org.junit.Test
+    @Test
     public void dodawanie() {
             DodawanieApplication dodawanieApplication=new DodawanieApplication();
             assertEquals(10,dodawanieApplication.dodawanie(5,5));
